4a3cdb2da3d782f8085644d9c7ecf77879844d42
John Franklin Crenshaw <jfc20@uw.edu>
2021-01-07 22:25:26 -0800

    Fixed some log determinant calculations, and added test for Scale bijector.

--- pzflow/bijectors.py
+++ pzflow/bijectors.py
@@ -2,103 +2,6 @@ import jax.numpy as np
 from jax import random, ops
 
 
-def Scale(scale):
-    def init_fun(rng, input_dim, **kwargs):
-        def forward_fun(params, inputs, **kwargs):
-            return scale * inputs, np.log(np.abs(scale)) * np.ones(inputs.shape[0])
-
-        def inverse_fun(params, inputs, **kwargs):
-            return inputs / scale, np.log(np.abs(1 / scale)) * np.ones(inputs.shape[0])
-
-        return (), forward_fun, inverse_fun
-
-    return init_fun
-
-
-def Reverse():
-    def init_fun(rng, input_dim, **kwargs):
-        def forward_fun(params, inputs, **kwargs):
-            return inputs[:, ::-1], np.zeros(inputs.shape[0])
-
-        def inverse_fun(params, inputs, **kwargs):
-            return inputs[:, ::-1], np.zeros(inputs.shape[0])
-
-        return (), forward_fun, inverse_fun
-
-    return init_fun
-
-
-def Shuffle():
-    def init_fun(rng, input_dim, **kwargs):
-
-        perm = random.permutation(rng, np.arange(input_dim))
-        inv_perm = np.argsort(perm)
-
-        def forward_fun(params, inputs, **kwargs):
-            return inputs[:, perm], np.zeros(inputs.shape[0])
-
-        def inverse_fun(params, inputs, **kwargs):
-            return inputs[:, inv_perm], np.zeros(inputs.shape[0])
-
-        return (), forward_fun, inverse_fun
-
-    return init_fun
-
-
-def Roll(shift=1):
-    def init_fun(rng, input_dim, **kwargs):
-        def forward_fun(params, inputs, **kwargs):
-            return np.roll(inputs, shift=shift, axis=-1), np.zeros(inputs.shape[0])
-
-        def inverse_fun(params, inputs, **kwargs):
-            return np.roll(inputs, shift=-shift, axis=-1), np.zeros(inputs.shape[0])
-
-        return (), forward_fun, inverse_fun
-
-    return init_fun
-
-
-def ColorTransform(ref_idx, ref_mean, ref_stdd):
-    def init_fun(rng, input_dim, **kwargs):
-        def forward_fun(params, inputs, **kwargs):
-            # calculate reference magnitude,
-            # and convert all colors to be in terms of the first magnitude, mag[0]
-            outputs = np.hstack(
-                (
-                    inputs[:, 0, None],  # redshift unchanged
-                    inputs[:, 1, None] * ref_stdd + ref_mean,  # reference mag
-                    np.cumsum(inputs[:, 2:], axis=-1),  # all colors --> mag[0] - mag[i]
-                )
-            )
-            # calculate mag[0]
-            outputs = ops.index_update(
-                outputs, ops.index[:, 1], outputs[:, 1] + outputs[:, ref_idx]
-            )
-            # mag[i] = mag[0] - (mag[0] - mag[i])
-            outputs = ops.index_update(
-                outputs, ops.index[:, 2:], outputs[:, 1, None] - outputs[:, 2:]
-            )
-
-            log_det = 1 / np.log(1 / ref_stdd) * np.ones(inputs.shape[0])
-
-            return outputs, log_det
-
-        def inverse_fun(params, inputs, **kwargs):
-            outputs = np.hstack(
-                (
-                    inputs[:, 0, None],  # redshift
-                    (inputs[:, ref_idx, None] - ref_mean) / ref_stdd,  # ref mag
-                    -np.diff(inputs[:, 1:]),  # colors
-                )
-            )
-            log_det = np.log(1 / ref_stdd) * np.ones(inputs.shape[0])
-            return outputs, log_det
-
-        return (), forward_fun, inverse_fun
-
-    return init_fun
-
-
 def Chain(*init_funs):
     def init_fun(rng, input_dim, **kwargs):
 
@@ -125,5 +28,104 @@ def Chain(*init_funs):
             return bijector_chain(params[::-1], inverse_funs[::-1], inputs)
 
         return all_params, forward_fun, inverse_fun
+
+    return init_fun
+
+
+def ColorTransform(ref_idx, ref_mean, ref_stdd):
+    def init_fun(rng, input_dim, **kwargs):
+        def forward_fun(params, inputs, **kwargs):
+            # calculate reference magnitude,
+            # and convert all colors to be in terms of the first magnitude, mag[0]
+            outputs = np.hstack(
+                (
+                    inputs[:, 0, None],  # redshift unchanged
+                    inputs[:, 1, None] * ref_stdd + ref_mean,  # reference mag
+                    np.cumsum(inputs[:, 2:], axis=-1),  # all colors --> mag[0] - mag[i]
+                )
+            )
+            # calculate mag[0]
+            outputs = ops.index_update(
+                outputs, ops.index[:, 1], outputs[:, 1] + outputs[:, ref_idx]
+            )
+            # mag[i] = mag[0] - (mag[0] - mag[i])
+            outputs = ops.index_update(
+                outputs, ops.index[:, 2:], outputs[:, 1, None] - outputs[:, 2:]
+            )
+            log_det = np.log(ref_stdd) * np.ones(inputs.shape[0])
+            return outputs, log_det
+
+        def inverse_fun(params, inputs, **kwargs):
+            outputs = np.hstack(
+                (
+                    inputs[:, 0, None],  # redshift
+                    (inputs[:, ref_idx, None] - ref_mean) / ref_stdd,  # ref mag
+                    -np.diff(inputs[:, 1:]),  # colors
+                )
+            )
+            log_det = -np.log(ref_stdd) * np.ones(inputs.shape[0])
+            return outputs, log_det
+
+        return (), forward_fun, inverse_fun
+
+    return init_fun
+
+
+def Reverse():
+    def init_fun(rng, input_dim, **kwargs):
+        def forward_fun(params, inputs, **kwargs):
+            return inputs[:, ::-1], np.zeros(inputs.shape[0])
+
+        def inverse_fun(params, inputs, **kwargs):
+            return inputs[:, ::-1], np.zeros(inputs.shape[0])
+
+        return (), forward_fun, inverse_fun
+
+    return init_fun
+
+
+def Roll(shift=1):
+    def init_fun(rng, input_dim, **kwargs):
+        def forward_fun(params, inputs, **kwargs):
+            return np.roll(inputs, shift=shift, axis=-1), np.zeros(inputs.shape[0])
+
+        def inverse_fun(params, inputs, **kwargs):
+            return np.roll(inputs, shift=-shift, axis=-1), np.zeros(inputs.shape[0])
+
+        return (), forward_fun, inverse_fun
+
+    return init_fun
+
+
+def Scale(scale):
+    def init_fun(rng, input_dim, **kwargs):
+        def forward_fun(params, inputs, **kwargs):
+            outputs = scale * inputs
+            log_det = np.log(scale ** inputs.shape[-1]) * np.ones(inputs.shape[0])
+            return outputs, log_det
+
+        def inverse_fun(params, inputs, **kwargs):
+            outputs = 1 / scale * inputs
+            log_det = -np.log(scale ** inputs.shape[-1]) * np.ones(inputs.shape[0])
+            return outputs, log_det
+
+        return (), forward_fun, inverse_fun
+
+    return init_fun
+
+
+def Shuffle():
+    def init_fun(rng, input_dim, **kwargs):
+
+        perm = random.permutation(rng, np.arange(input_dim))
+        inv_perm = np.argsort(perm)
+
+        def forward_fun(params, inputs, **kwargs):
+            return inputs[:, perm], np.zeros(inputs.shape[0])
+
+        def inverse_fun(params, inputs, **kwargs):
+            return inputs[:, inv_perm], np.zeros(inputs.shape[0])
+
+        return (), forward_fun, inverse_fun
 
     return init_fun
--- tests/test_bijectors.py
+++ tests/test_bijectors.py
@@ -1,4 +1,4 @@
-from pzflow.bijectors import Reverse
+from pzflow.bijectors import Reverse, Scale
 import jax.numpy as np
 from jax import random
 
@@ -17,3 +17,21 @@ def test_Reverse():
     outputs, log_det = inverse_fun(params, outputs)
     assert np.allclose(outputs, x)
     assert np.allclose(log_det, np.zeros(x.shape[0]))
+
+
+def test_Scale():
+    rng = random.PRNGKey(0)
+    x = np.array([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]])
+
+    init_fun = Scale(scale=2)
+    params, forward_fun, inverse_fun = init_fun(rng, x.shape[-1])
+    outputs, fwd_log_det = forward_fun(params, x)
+
+    assert np.allclose(outputs, 2 * x)
+    assert np.allclose(fwd_log_det, np.log(2 ** x.shape[-1]) * np.ones(x.shape[0]))
+
+    outputs, inv_log_det = inverse_fun(params, outputs)
+    assert np.allclose(outputs, x)
+    assert np.allclose(inv_log_det, -np.log(2 ** x.shape[-1]) * np.ones(x.shape[0]))
+
+    assert np.allclose(fwd_log_det + inv_log_det, np.zeros(fwd_log_det.shape))
