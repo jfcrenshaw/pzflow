1ac23973419c654d71cdbf519f14774c728d3457
John Franklin Crenshaw <jfc20@uw.edu>
2021-01-12 16:28:54 -0800

    Added two moons example data as well

--- pzflow/examples/examples.py
+++ pzflow/examples/examples.py
@@ -3,7 +3,15 @@ from pzflow import Flow
 import os
 
 
-def example_data():
+def two_moons_data():
+    this_dir, _ = os.path.split(__file__)
+    data_path = os.path.join(this_dir, "two-moons-data.npy")
+    columns = ("x", "y")
+    data = np.load(data_path)
+    return columns, data
+
+
+def galaxy_data():
     this_dir, _ = os.path.split(__file__)
     data_path = os.path.join(this_dir, "galaxy-data.npy")
     columns = ("redshift", "u", "g", "r", "i", "z", "y")
@@ -17,7 +25,7 @@ def example_flow():
     flow = Flow(file=flow_path)
     flow.info = """
                 This is an example flow, trained on 100,000 simulated galaxies with 
-                redshifts in the range (0,2.3) and photometry in the LSST ugrizy bands.
+                redshifts in the range (0, 2.3) and photometry in the LSST ugrizy bands.
 
                 The flow expects (and produces) arrays with columns in the order
                 (redshift, u, g, r, i, z, y)
