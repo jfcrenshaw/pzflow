b44a4f53522fd827c30be480d1ddde62c7954af8
John Franklin Crenshaw <jfc20@uw.edu>
2021-01-13 21:09:02 -0800

    Added LSSTErrorModel

--- pzflow/utils.py
+++ pzflow/utils.py
@@ -2,6 +2,8 @@ import jax.numpy as np
 import numpy as onp
 from jax import random
 from jax.scipy.stats import multivariate_normal
+from numbers import Number
+import pandas as pd
 
 
 class Normal:
@@ -23,3 +25,95 @@ class Normal:
             cov=np.identity(self.input_dim),
             shape=(nsamples,),
         )
+
+
+class LSSTErrorModel:
+    def __init__(
+        self,
+        limiting_mags: dict = None,
+        err_params: dict = None,
+        undetected_flag: int = 99,
+    ):
+
+        if isinstance(limiting_mags, dict):
+            self.limiting_mags = limiting_mags
+        elif limiting_mags is not None:
+            raise ValueError("limiting_mags must be a dictionary")
+        else:
+            # defaults are 10 year 5-sigma point source depth
+            # from https://www.lsst.org/scientists/keynumbers
+            self.limiting_mags = {
+                "u": 26.1,
+                "g": 27.4,
+                "r": 27.5,
+                "i": 26.8,
+                "z": 26.1,
+                "y": 27.9,
+            }
+
+        if isinstance(err_params, dict):
+            self.err_params = err_params
+        elif err_params is not None:
+            raise ValueError("err_params must be a dictionary")
+        else:
+            # defaults are gamma values in Table 2
+            # from https://arxiv.org/pdf/0805.2366.pdf
+            self.err_params = {
+                "u": 0.038,
+                "g": 0.039,
+                "r": 0.039,
+                "i": 0.039,
+                "z": 0.039,
+                "y": 0.039,
+            }
+
+        self.undetected_flag = undetected_flag
+
+        # check that the keys match
+        err_str = "limiting_mags and err_params have different keys"
+        assert self.limiting_mags.keys() == self.err_params.keys(), err_str
+
+        # check that all values are numbers
+        all_numbers = all(
+            isinstance(val, Number) for val in self.limiting_mags.values()
+        )
+        err_str = "All limiting magnitudes must be numbers"
+        assert all_numbers, err_str
+        all_numbers = all(isinstance(val, Number) for val in self.err_params.values())
+        err_str = "All error parameters must be numbers"
+        assert all_numbers, err_str
+        all_numbers = isinstance(self.undetected_flag, Number)
+        err_str = (
+            "The undetected flag for mags beyond the 5-sigma limit must be a number"
+        )
+        assert all_numbers, err_str
+
+    def __call__(self, data: pd.DataFrame, seed: int = None) -> pd.DataFrame:
+        # Gaussian errors using Equation 5
+        # from https://arxiv.org/pdf/0805.2366.pdf
+        # then flag all magnitudes beyond 5-sig limit
+
+        data = data.copy()
+
+        rng = onp.random.default_rng(seed)
+
+        for band in self.limiting_mags.keys():
+
+            # calculate err with Eq 5
+            m5 = self.limiting_mags[band]
+            gamma = self.err_params[band]
+            x = 10 ** (0.4 * (data[band] - m5))
+            err = onp.sqrt((0.04 - gamma) * x + gamma * x ** 2)
+
+            # Add errs to galaxies within limiting mag
+            data[f"{band}_err"] = err
+            rand_err = rng.normal(0, err)
+            rand_err[data[band] > m5] = 0
+            data[band] += rand_err
+
+            # flag mags beyond limiting mag
+            data.loc[
+                data.eval(f"{band} > {m5}"), (band, f"{band}_err")
+            ] = self.undetected_flag
+
+        return data
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,10 @@
+import pytest
 import jax.numpy as np
-from pzflow.utils import Normal
+from pzflow.utils import Normal, LSSTErrorModel
+import pandas as pd
 
 
-def test_returns_correct_shapes():
+def test_Normal_returns_correct_shapes():
 
     input_dim = 2
     normal = Normal(input_dim)
@@ -15,7 +17,7 @@ def test_returns_correct_shapes():
     assert log_prob.shape == (nsamples,)
 
 
-def test_control_sample_randomness():
+def test_Normal_control_sample_randomness():
 
     input_dim = 2
     normal = Normal(input_dim)
@@ -28,3 +30,49 @@ def test_control_sample_randomness():
     s1 = normal.sample(nsamples, seed=0)
     s2 = normal.sample(nsamples, seed=0)
     assert np.allclose(s1, s2)
+
+
+@pytest.mark.parametrize(
+    "limiting_mags,err_params",
+    [
+        ("fake", None),
+        (None, "fake"),
+    ],
+)
+def test_LSSTErrorModel_inputs_not_dictionary(limiting_mags, err_params):
+    with pytest.raises(ValueError):
+        err_model = LSSTErrorModel(limiting_mags=limiting_mags, err_params=err_params)
+
+
+@pytest.mark.parametrize(
+    "limiting_mags,err_params,undetected_flag",
+    [
+        ({"u": "fake", "g": 1}, {"u": 1, "g": 1}, 99),
+        ({"u": 1, "g": 1}, {"u": "fake", "g": 1}, 99),
+        ({"u": 1, "g": 1}, {"u": 1, "g": 1}, "fake"),
+        ({"u": 1, "g": 1}, {"u": 1}, 99),
+        ({"u": 1}, {"u": 1, "g": 1}, 99),
+    ],
+)
+def test_LSSTErrorModel_bad_inputs(limiting_mags, err_params, undetected_flag):
+    with pytest.raises(AssertionError):
+        err_model = LSSTErrorModel(
+            limiting_mags=limiting_mags,
+            err_params=err_params,
+            undetected_flag=undetected_flag,
+        )
+
+
+def test_LSSTErrorModel_returns_correct_shape():
+    data = pd.DataFrame(
+        [
+            [0.1, 24, 25, 26, 26, 27, 28],
+            [0.2, 25, 24, 24, 25, 24, 23],
+            [1.2, 26, 26, 25, 25, 27, 28],
+        ],
+        columns=["redshift", "u", "g", "r", "i", "z", "y"],
+    )
+
+    err_data = LSSTErrorModel()(data)
+
+    assert err_data.shape == (data.shape[0], 2 * data.shape[1] - 1)
