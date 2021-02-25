3a0c3a9d673d2b289bc589ecedb016a41b250e7c
John Franklin Crenshaw <jfc20@uw.edu>
2021-02-24 19:10:09 -0800

    Fixed bijector tests.

--- tests/test_bijectors.py
+++ tests/test_bijectors.py
@@ -14,56 +14,63 @@ x = np.array(
 
 
 @pytest.mark.parametrize(
-    "bijector,args",
+    "bijector,args,conditions",
     [
-        (ColorTransform, (3, [1, 3, 5])),
-        (Reverse, ()),
-        (Roll, (2,)),
-        (Scale, (2.0,)),
-        (Shuffle, ()),
-        (InvSoftplus, (0,)),
-        (InvSoftplus, ([1, 3], [2.0, 12.0])),
-        (StandardScaler, (np.linspace(-1, 1, 7), np.linspace(1, 8, 7))),
-        (Chain, (Reverse(), Scale(1 / 6), Roll(-1))),
-        (NeuralSplineCoupling, ()),
-        (RollingSplineCoupling, (2,)),
+        (ColorTransform, (3, [1, 3, 5]), np.zeros((3, 1))),
+        (Reverse, (), np.zeros((3, 1))),
+        (Roll, (2,), np.zeros((3, 1))),
+        (Scale, (2.0,), np.zeros((3, 1))),
+        (Shuffle, (), np.zeros((3, 1))),
+        (InvSoftplus, (0,), np.zeros((3, 1))),
+        (InvSoftplus, ([1, 3], [2.0, 12.0]), np.zeros((3, 1))),
+        (
+            StandardScaler,
+            (np.linspace(-1, 1, 7), np.linspace(1, 8, 7)),
+            np.zeros((3, 1)),
+        ),
+        (Chain, (Reverse(), Scale(1 / 6), Roll(-1)), np.zeros((3, 1))),
+        (NeuralSplineCoupling, (), np.zeros((3, 1))),
+        (NeuralSplineCoupling, (16, 3, 2, 128, 3), np.arange(9).reshape(3, 3)),
+        (RollingSplineCoupling, (2,), np.zeros((3, 1))),
     ],
 )
 class TestBijectors:
-    def test_returns_correct_shape(self, bijector, args):
+    def test_returns_correct_shape(self, bijector, args, conditions):
         init_fun, bijector_info = bijector(*args)
         params, forward_fun, inverse_fun = init_fun(random.PRNGKey(0), x.shape[-1])
 
-        fwd_outputs, fwd_log_det = forward_fun(params, x)
+        fwd_outputs, fwd_log_det = forward_fun(params, x, conditions=conditions)
         assert fwd_outputs.shape == x.shape
         assert fwd_log_det.shape == x.shape[:1]
 
-        inv_outputs, inv_log_det = inverse_fun(params, x)
+        inv_outputs, inv_log_det = inverse_fun(params, x, conditions=conditions)
         assert inv_outputs.shape == x.shape
         assert inv_log_det.shape == x.shape[:1]
 
-    def test_is_bijective(self, bijector, args):
+    def test_is_bijective(self, bijector, args, conditions):
         init_fun, bijector_info = bijector(*args)
         params, forward_fun, inverse_fun = init_fun(random.PRNGKey(0), x.shape[-1])
 
-        fwd_outputs, fwd_log_det = forward_fun(params, x)
-        inv_outputs, inv_log_det = inverse_fun(params, fwd_outputs)
+        fwd_outputs, fwd_log_det = forward_fun(params, x, conditions=conditions)
+        inv_outputs, inv_log_det = inverse_fun(
+            params, fwd_outputs, conditions=conditions
+        )
 
         print(inv_outputs)
         assert np.allclose(inv_outputs, x, atol=1e-6)
         assert np.allclose(fwd_log_det, -inv_log_det, atol=1e-6)
 
-    def test_is_jittable(self, bijector, args):
+    def test_is_jittable(self, bijector, args, conditions):
         init_fun, bijector_info = bijector(*args)
         params, forward_fun, inverse_fun = init_fun(random.PRNGKey(0), x.shape[-1])
 
-        fwd_outputs_1, fwd_log_det_1 = forward_fun(params, x)
+        fwd_outputs_1, fwd_log_det_1 = forward_fun(params, x, conditions=conditions)
         forward_fun = jit(forward_fun)
-        fwd_outputs_2, fwd_log_det_2 = forward_fun(params, x)
+        fwd_outputs_2, fwd_log_det_2 = forward_fun(params, x, conditions=conditions)
 
-        inv_outputs_1, inv_log_det_1 = inverse_fun(params, x)
+        inv_outputs_1, inv_log_det_1 = inverse_fun(params, x, conditions=conditions)
         inverse_fun = jit(inverse_fun)
-        inv_outputs_2, inv_log_det_2 = inverse_fun(params, x)
+        inv_outputs_2, inv_log_det_2 = inverse_fun(params, x, conditions=conditions)
 
 
 @pytest.mark.parametrize(
